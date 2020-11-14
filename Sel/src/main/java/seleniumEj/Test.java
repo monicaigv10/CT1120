c9e9d66144f7fed80f64c7d498e29368a08ada00
monicaigv10 <64491533+monicaigv10@users.noreply.github.com>
2020-11-14 07:17:52 -0800

    Update Test.java
    update classes

--- Sel/src/main/java/seleniumEj/Test.java
+++ Sel/src/main/java/seleniumEj/Test.java
@@ -10,6 +10,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
 		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; //Setando el path de nuestro driver
 		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver"); //Identificando el .exe para driver
 		WebDriver driver = new ChromeDriver(); //Creacion en tiempo de ejecucion de nuestro chromedriver
